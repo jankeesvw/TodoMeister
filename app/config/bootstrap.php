e82909efec89cf575d10d10c0f02d3a876cf2252
Jankees van Woezik <jankees@base42.nl>
2011-04-18 11:45:22 +0200

    Version 1.5d - Update to the routes to open a todo list without a username (redirects to 'start')

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
-Configure::write('TodoMeister.version','1.5c');
+Configure::write('TodoMeister.version','1.5d');
 Configure::write('TodoMeister.build_index',false); // true or false
 Configure::write('TodoMeister.show_feedback',true); // true or false
