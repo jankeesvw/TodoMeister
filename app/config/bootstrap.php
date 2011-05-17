ea71835f55799367cc661354887b2102a9153f9d
Jankees van Woezik <jankees@base42.nl>
2011-05-17 13:54:47 +0200

    Version 1.5f - Sanitize user input in todo items

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
-Configure::write('TodoMeister.version','1.5e');
+Configure::write('TodoMeister.version','1.5f');
 Configure::write('TodoMeister.build_index',false); // true or false
 Configure::write('TodoMeister.show_feedback',true); // true or false
