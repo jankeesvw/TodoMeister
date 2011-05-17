4684caa22d484ad94a110aa3082a635073fc3f33
Jankees van Woezik <jankees@base42.nl>
2011-05-17 13:51:56 +0200

    Sanitize everything in the before save

--- app/models/todo.php
+++ app/models/todo.php
@@ -1,6 +1,9 @@
 <?php
 
+   App::import('Sanitize');
+
 	class Todo extends AppModel {
+	   
 	    var $name = 'Todo';
 		var $actsAs = array('Revision' => array('ignore'=>array('modified','order','color')));
 	    var $validate = array(
@@ -15,6 +18,16 @@
 		    ),
 
 	    );
+	    
+	    function beforeSave()
+        {
+          //$this->data['Event']['start'] = $this->_getDate('Event', 'start');
+          
+          echo $this->data['Todo']['text'];
+          $this->data['Todo']['text'] = Sanitize::clean($this->data['Todo']['text'], array('encode' => true,'remove_html' => true));;
+          echo $this->data['Todo']['text'];
+          return true;
+        }
 		
 	}
 
